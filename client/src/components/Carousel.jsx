4bae61141b34823d7a38a8b88b7b6d4732c5647a
zhliang0204 <zhiliang0204@gmail.com>
2019-06-15 21:48:54 +0200

    update carousel

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -2,6 +2,35 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 
 class Carousel extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      curTag:0
+    }
+  }
+
+  toggle(e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e)
+    let curid = parseInt(e.target.id.split("-")[1])
+console.log(curid)
+    this.setState({
+      curTag:curid
+    })
+  }
+
+  goBack(e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e)
+    // let curid = parseInt(e.target.id.split("-")[1])
+
+    this.setState({
+      curTag:0
+    })
+  }
+
   render() {
     var settings = {
       dots: true,
@@ -16,21 +45,58 @@ class Carousel extends Component {
       pauseOnHover: true
     };
     return (
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-          <img />
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-      </Slider>
+      <div>
+         {this.state.curTag === 0 &&(
+          <Slider {...settings}>
+            <div >
+              <h3 id="pic-1" onClick={(e) => this.toggle(e)}>1</h3>
+              {/* <img/> */}
+            </div>
+            <div >
+              <h3 id="pic-2" onClick={(e) => this.toggle(e)}>2</h3>
+            </div>
+            <div >
+              <h3 id="pic-3" onClick={(e) => this.toggle(e)}>3</h3>
+            </div>
+            <div >
+              <h3 id="pic-4" onClick={(e) => this.toggle(e)}>4</h3>
+            </div>
+          </Slider>
+         )}
+
+         {this.state.curTag === 1 && (
+           <div>
+             <div onClick={(e)=>this.goBack(e)}>go back</div>
+             111111
+
+           </div>
+         )}
+
+        {this.state.curTag === 2 && (
+           <div>
+             <div onClick={(e)=>this.goBack(e)}>go back</div>
+             22222
+          </div>
+        )}
+
+        {this.state.curTag === 3 && (
+           <div>
+             <div onClick={(e)=>this.goBack(e)}>go back</div>
+             33333
+             
+          </div>
+        )}
+
+        {this.state.curTag === 4 && (
+           <div>
+             <div onClick={(e)=>this.goBack(e)}>go back</div>
+             444444
+             
+          </div>
+        )}
+      </div>
+      
+      
     );
   }
 }
