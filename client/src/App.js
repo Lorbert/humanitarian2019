1b68cf347b165098761d8835d2fed71a3fa8efd0
Johanna S <johanna.skuhra@incapptic.com>
2019-06-15 15:43:51 +0200

    restructuring client folder again

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import PollutionMap from "./components/PollutionMap";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      Backpacker's Guide Through Worldwide Textile Water Pollution
+      <NavBar />
+      <PollutionMap />
+    </div>
+  );
 }
 
 export default App;
