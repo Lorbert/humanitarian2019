874dde851bdd36ec96adb936ae73d4111029b7f8
zhliang0204 <zhiliang0204@gmail.com>
2019-06-15 23:53:43 +0200

    participate information

---
+++ client/src/components/Participate.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Participate extends Component {
+    render() {
+        return (
+            <div className="outer-div">
+                <div className="sub-div">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="sub-div">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="sub-div">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="sub-div">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
+}
