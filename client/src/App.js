223b13b8d4e83fdfc1bd19a09e7ab556615759be
Johanna S <johanna.skuhra@incapptic.com>
2019-06-15 16:23:50 +0200

    first steps in css

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
-      Backpacker's Guide Through Worldwide Textile Water Pollution
+      <h1 className="backpackers-title">
+        Backpacker's Guide Through Worldwide Textile Water Pollution
+      </h1>
       <NavBar />
       <PollutionMap />
     </div>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class NavBar extends Component {
   render() {
-    return <div>I am a Navbar</div>;
+    return <div>I am a Navbar, maybe ...</div>;
   }
 }
